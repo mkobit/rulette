8a3ffe69f9f3b37dc10e09014a24ce3593969674
Mike Kobit <mkobit@gmail.com>
2026-09-13 23:25:44 -0500

    refactor: remove internal sbx models and adopt sbx cli

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ pub mod ir;
 pub mod parsers;
 pub mod pipeline;
 pub mod publication;
-pub mod sbx;
 
 pub use compilation::{compile, lower_unique_targets, CompilationRequest};
 pub use emitters::lowering::{
